2e213e395c7f190290fea1f6456ae31ad77afc97
Klas Mellbourn <klas@mellbourn.net>
2023-05-05 20:02:21 +0200

    Update Install-Dotfiles.ps1
    more programs

--- Install-Dotfiles.ps1
+++ Install-Dotfiles.ps1
@@ -1,6 +1,8 @@
 winget install -e --id Intel.IntelDriverAndSupportAssistant
 winget install -e --id Git.Git
-winget install -e --id Google.Chrome.Beta
+winget install -e --id Google.Chrome
 winget install -e --id Bitwarden.Bitwarden
 # winget install -e --id Bitwarden.CLI
 # winget install -e --id DominikReichl.KeePass
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id Valve.Steam
