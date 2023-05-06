f8d67a1ac3ccd14643f46bda69266a29dfafdf97
Klas Mellbourn <klas@mellbourn.net>
2023-05-06 19:44:43 +0200

    Update Install-Dotfiles.ps1
    add croc
    comment out DirectX

--- Install-Dotfiles.ps1
+++ Install-Dotfiles.ps1
@@ -2,10 +2,10 @@ winget install -e --id Intel.IntelDriverAndSupportAssistant
 winget install -e --id Git.Git
 winget install -e --id Google.Chrome
 winget install -e --id Bitwarden.Bitwarden
-# winget install -e --id Bitwarden.CLI
 # winget install -e --id DominikReichl.KeePass
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Valve.Steam
 winget install -e --id Microsoft.PowerShell
-# without this Everquests throws
-winget install -e --id Microsoft.DirectX
+# without this Everquests throws Missing d3dx9_43.dll
+#winget install -e --id Microsoft.DirectX
+winget install -e --id schollz.croc
