11ece4baddc947c08f830cbd1799d811c5eae675
Klas Mellbourn <klas@mellbourn.net>
2023-05-06 19:45:57 +0200

    Update Install-Dotfiles.ps1
    improve comments

--- Install-Dotfiles.ps1
+++ Install-Dotfiles.ps1
@@ -6,6 +6,6 @@ winget install -e --id Bitwarden.Bitwarden
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Valve.Steam
 winget install -e --id Microsoft.PowerShell
-# without this Everquests throws Missing d3dx9_43.dll
+# without this Everquests throws Missing d3dx9_43.dll. Note, winget fails to detect this being installed
 #winget install -e --id Microsoft.DirectX
 winget install -e --id schollz.croc
