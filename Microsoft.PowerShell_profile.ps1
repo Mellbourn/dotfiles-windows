2c8c2dfe6d24f3e595bfa77e22dc0e09bcccc23d
Klas Mellbourn <klas@mellbourn.net>
2023-05-20 13:46:00 +0200

    better path

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,7 @@
 # this script is used by PowerShell 7
 
-$env:Path += ";$env:USERPROFILE\bin"
+# should I set the path to $PSScriptRoot/Scripts instead?
+$env:Path += ";$env:USERPROFILE\bin;$PSScriptRoot"
 
 Set-Alias g git
 function yb {
