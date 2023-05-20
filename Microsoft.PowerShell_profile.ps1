571775d7537ddc790d382e1fdb2f729056f0a3af
Klas Mellbourn <klas@mellbourn.net>
2023-05-20 13:27:47 +0200

    PSReadLineOptions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -12,3 +12,6 @@ Import-Module z
 oh-my-posh init pwsh | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
+
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -PredictionViewStyle ListView
