f05d73b185552c36d741704d89b8fc2ccc6fcc8b
Klas Mellbourn <klas@mellbourn.net>
2023-06-01 01:12:37 +0200

    move git crypt installation to separate script

---
+++ MyScripts/Install-BinPrograms.ps1
@@ -0,0 +1,13 @@
+[CmdletBinding()] param ()
+$ErrorActionPreference = "Stop"
+
+if (-Not (Test-Path -Path $env:USERPROFILE/bin)) {
+  New-Item -Type Directory -Path $env:USERPROFILE/bin
+}
+
+$gitCryptPattern = "git-crypt-*-x86_64.exe"
+if (-Not (Test-Path -Path $env:USERPROFILE/bin/$gitCryptPattern)) {
+  Write-Verbose "Installing git-crypt"
+  Install-LatestRelease -repoName AGWA/git-crypt -assetPattern $gitCryptPattern
+  sudo New-Item -ItemType SymbolicLink -Path $env:USERPROFILE/bin/git-crypt.exe -Target bin/$gitCryptPattern
+}
--- MyScripts/Install-Dotfiles.ps1
+++ MyScripts/Install-Dotfiles.ps1
@@ -13,16 +13,7 @@ attrib +p $PSScriptRoot\..\*.* /s
 
 Install-WingetPackages @args
 
-if (-Not (Test-Path -Path $env:USERPROFILE/bin)) {
-  New-Item -Type Directory -Path $env:USERPROFILE/bin
-}
-
-$gitCryptPattern = "git-crypt-*-x86_64.exe"
-if (-Not (Test-Path -Path $env:USERPROFILE/bin/$gitCryptPattern)) {
-  Write-Verbose "Installing git-crypt"
-  Install-LatestRelease -repoName AGWA/git-crypt -assetPattern $gitCryptPattern
-  sudo New-Item -ItemType SymbolicLink -Path $env:USERPROFILE/bin/git-crypt.exe -Target bin/$gitCryptPattern
-}
+Install-BinPrograms @args
 
 Install-Modules @args
 
