f2b35fb5d54b7c85634c564685c3c31fc8d23f13
Klas Mellbourn <klas@mellbourn.net>
2023-06-01 00:38:10 +0200

    Move installing modules to separate file

--- MyScripts/Install-Dotfiles.ps1
+++ MyScripts/Install-Dotfiles.ps1
@@ -26,40 +26,7 @@ if (-Not (Test-Path -Path bin/$gitCryptPattern)) {
   sudo New-Item -ItemType SymbolicLink -Path bin/git-crypt.exe -Target bin/$gitCryptPattern
 }
 
-if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {
-  Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-}
-Write-Verbose "Install modules"
-$ModuleListString = "
-# Remove-ItemSafely, i.e. deletion by moving to the trash
-Recycle
-
-# add icons to ls, like lsd
-Terminal-Icons
-
-# unix-like environment
-z,PSFzf
-
-# this is needed only for command line completion in PSFzf
-posh-git
-"
-# Weirdly, listing the modules is more reliable than Get-Module or Get-InstalledModule
-$GetModuleList = (Get-ChildItem $PSScriptRoot\..\Modules).Name
-$ModuleList = $ModuleListString -split "`n"
-foreach ($ModuleLineString in $ModuleList) {
-  $ModuleLine = $ModuleLineString.Trim()
-  if ($ModuleLine -ne "" -and $ModuleLine -notlike "#*") {
-    $ModuleArray = $ModuleLine.Split(",")
-    foreach ($ModuleWithSpace in $ModuleArray) {
-      $Module = $ModuleWithSpace.Trim()
-      if ($Module -ne "" -and (-Not ($GetModuleList.Contains($Module)))) {
-        Write-Verbose "Installing '$Module'"
-        Install-Module -Name $Module -Repository PSGallery -Scope CurrentUser
-      }
-    }
-  }
-}
-
+Install-Modules @args
 
 # non installation configuration
 if (-Not (Test-Path -Path $env:CodeDir/private)) {
